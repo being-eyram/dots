372398f737d36ac417339b53e529d01e07636fc1
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2024-06-02 15:30:39 +0000

    chore: Add Database tables

--- src/main/kotlin/io/eyram/Application.kt
+++ src/main/kotlin/io/eyram/Application.kt
@@ -1,5 +1,8 @@
 package io.eyram
 
+import io.eyram.db.DatabaseFactory
+import io.eyram.db.models.Notes
+import io.eyram.db.models.Users
 import io.eyram.routes.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -8,16 +11,21 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
     embeddedServer(
         Netty, port = 8080,
         host = "127.0.0.1",
-        module = Application::mainModule
+        module = Application::mainModule,
     ).start(wait = true)
 }
 
 fun Application.mainModule() {
+    initializeDB()
     configurePlugins()
 
     routing {
@@ -33,5 +41,12 @@ fun Application.configurePlugins() {
             prettyPrint = true
         }
         json(json = jsonBuilder)
+    }
+}
+
+fun initializeDB() {
+    transaction(DatabaseFactory.db) {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(Users, Notes)
     }
 }
--- src/main/kotlin/io/eyram/routes/Dots.kt
+++ src/main/kotlin/io/eyram/routes/Dots.kt
@@ -1,10 +1,13 @@
 package io.eyram.routes
 
+import io.eyram.db.DatabaseFactory
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.dotRoutes(){
+fun Route.dotRoutes() {
+    val database = DatabaseFactory.db
+
     post("/api/dot") {
         call.respond(mapOf("hello" to "world"))
     }
@@ -13,19 +16,19 @@ fun Route.dotRoutes(){
         call.respondText("get all notes")
     }
 
-    get("/api/dots/{id}"){
+    get("/api/dots/{id}") {
 
         val noteId = call.parameters["id"]
         call.respondText("get note with id : $noteId")
 
     }
 
-    put ("/api/dots/{id}"){
+    put("/api/dots/{id}") {
         val noteId = call.parameters["id"]
         call.respondText("update note with id : $noteId")
     }
 
-    get ("/api/dots/search"){
+    get("/api/dots/search") {
         call.respondText("search results")
     }
 }
