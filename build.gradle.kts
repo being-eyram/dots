5d9352088897c1b758a0c5968a570c362004090d
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2024-06-02 15:31:33 +0000

    chore: Add exposed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -36,4 +37,13 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
+    implementation("org.postgresql:postgresql:42.7.3")
+    //implementation("org.jetbrains.exposed:exposed-crypt:$exposed_version")
 }
